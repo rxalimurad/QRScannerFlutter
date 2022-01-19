339916a2622662f6e14bf0401359aac01a7dc2ae
alimurad <amurad@i2cinc.com>
2022-01-19 16:44:20 +0500

    -

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -1,18 +1,19 @@
 import 'dart:developer';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
-import 'package:qr_scan_generator/DBHandler.dart';
-import 'package:qr_scan_generator/util.dart';
+import 'package:crop/crop.dart';
+
 import 'package:r_scan/r_scan.dart';
-import 'package:url_launcher/url_launcher.dart';
+
 
 import 'DataCacheManager.dart';
+import 'alert_dialog.dart';
 import 'main.dart';
 
 class QRViewExample extends StatefulWidget {
@@ -74,10 +75,14 @@ class _QRViewExampleState extends State<QRViewExample> {
                   final XFile? image =
                       await _picker.pickImage(source: ImageSource.gallery);
                   final result = await RScan.scanImagePath(image!.path);
+                  final controller = CropController();
+                  // controller.
+                  print(",,..${result?.points}");
                   if (result.message == null) {
-                    showAlertDialog("Empty", "" );
+
+                    AlertPopup.showAlertDialog("Empty", "" ,context, null);
                   } else {
-                    showAlertDialog("${result.type}", result.message! );
+                    AlertPopup.showAlertDialog("${result.type}", result.message!, context ,Image.memory(await image.readAsBytes()));
                   }
 
 
@@ -113,89 +118,18 @@ class _QRViewExampleState extends State<QRViewExample> {
     );
   }
 
-  String getButtonTitle(String content) {
-    if (isUrl(content)) {
-      return "Open URL";
-    } else if (GetUtils.isEmail(content)) {
-      return "Send Email";
-    } else if (GetUtils.isPhoneNumber(content)) {
-      return "Call Phone";
-    } else {
-      return "Copy to clipboard";
-    }
-  }
 
-  bool isUrl(String str) {
-    var urlPattern =
-        r"(https?|http)://([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?";
-    var match = new RegExp(urlPattern, caseSensitive: false).firstMatch(str);
-    print("matchmatch $match");
-    if (match == null) {
-      return false;
-    }
-    return !(match.isBlank ?? true);
-  }
 
-  Future _launchURL(String _url) async {
-    if (!await launch(_url)) throw 'Could not launch $_url';
-    return;
-  }
 
-  showAlertDialog(String title, String content) {
-    if (content.isNotEmpty) {
-      DBHandler.addData(QRHistory(0,content,Util.getDateNow()));
-    }
 
-    // set up the button
-    Widget okButton = TextButton(
-      child: Text(getButtonTitle(content)),
-      onPressed: () async {
-        DataCacheManager().showingPopup = false;
-        if (isUrl(content)) {
-          await _launchURL(content);
-          Navigator.pop(navigatorKey.currentContext!);
-        } else if (GetUtils.isEmail(content)) {
-          await _launchURL("mailto:$content");
-          Navigator.pop(navigatorKey.currentContext!);
-        } else if (GetUtils.isPhoneNumber(content)) {
-          await _launchURL("tel:$content");
-          Navigator.pop(navigatorKey.currentContext!);
-        } else {
-          Clipboard.setData(ClipboardData(text: content));
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Copied to clipboard')),
-          );
-          Navigator.pop(navigatorKey.currentContext!);
-        }
-      },
-    );
-    // set up the AlertDialog
-    AlertDialog alert = AlertDialog(
-      title: Text(title),
-      content: Text(content),
-      actions: [
-        okButton,
-      ],
-    );
-
-    // show the dialog
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        DataCacheManager().showingPopup = true;
-        return alert;
-      },
-    );
-  }
 
   void _onQRViewCreated(QRViewController controller) {
     setState(() {
       this.controller = controller;
     });
-    controller.scannedDataStream.listen((scanData) {
-      if (!DataCacheManager().showingPopup) {
-        showAlertDialog("${scanData.format}", "${scanData.code}");
+  controller.scannedDataStream.listen((scanData) {
+    if (!DataCacheManager().showingPopup) {
+        AlertPopup.showAlertDialog("${scanData.format}", "${scanData.code}", context,Image.memory(Uint8List.fromList(scanData.rawBytes!)));
         print(
             ",,..${scanData.code} ${scanData.format} ${DataCacheManager().showingPopup}");
       }
