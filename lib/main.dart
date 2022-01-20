f91fa67ae238f7d5c555917bef86daff023b3a7f
alimurad <amurad@i2cinc.com>
2022-01-20 18:13:25 +0500

    -

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,118 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:qr_scan_generator/scanner.dart';
 
+import 'controllers.dart';
+import 'custom_icons.dart';
 import 'generator.dart';
 import 'history.dart';
 
-final navigatorKey = GlobalKey<NavigatorState>();
+void main() => runApp(const ExampleApp());
 
-Future<void> main() async {
- runApp(MyApp());
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({Key? key}) : super(key: key);
 
-}
-
-class MyApp extends StatelessWidget {
-  final BottomController c = Get.put(BottomController());
-  
   @override
   Widget build(BuildContext context) {
-    c.selectedTab.value = 1;
-    return MaterialApp(
-      title: 'Flutter Demo',
-      navigatorKey: navigatorKey,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        bottomNavigationBar: Obx(() {
-          return CurvedNavigationBar(
-            backgroundColor: getColor(),
-            animationDuration: Duration(milliseconds: 200),
-            animationCurve: Curves.easeInCubic,
-            items: <Widget>[
-              Icon(Icons.scanner, size: 30),
-              Icon(Icons.add, size: 30),
-              Icon(Icons.history, size: 30),
-              Icon(Icons.person, size: 30),
-            ],
-            onTap: (index) {
-              // DataCacheManager().showingPopup = false;
-              c.selectedTab.value = index + 1;
-            },
-          );
-        }),
-        body: Stack(children: [
-          Obx(() {
-            return Visibility(
-              visible: c.selectedTab.value == 1,
-              child: Container(
-                color: getColor(),
-                child: QRViewExample(),
-              ),
-            );
-          }),
-          Obx(() {
-            return Visibility(
-              visible: c.selectedTab.value == 2,
-              child: Container(
-                color: getColor(),
-                child: QRGeneratorSharePage(),
-              ),
-            );
-          }),
-          Obx(() {
-            return Visibility(
-              visible: c.selectedTab.value == 3,
-              child: Container(
-                color: getColor(),
-                child: HistoryView(),
-              ),
-            );
-          }),
-          Obx(() {
-            return Visibility(
-              visible: c.selectedTab.value == 4,
-              child: Container(
-                color: getColor(),
-                child: Center(child: Text("Tab 4")),
-              ),
-            );
-          }),
-        ]),
-      ),
+    return const MaterialApp(
+      home: RootView(),
     );
   }
 }
 
-getColor() {
-  BottomController c = Get.find();
-  var index = c.selectedTab.value;
-  if (index == 1) {
-    return Colors.deepOrange;
-  } else if (index == 2) {
-    return Colors.deepPurple;
-  } else if (index == 3) {
-    return Colors.white;
-  } else {
-    return Colors.green;
-  }
+class RootView extends StatefulWidget {
+  const RootView({Key? key}) : super(key: key);
+
+  @override
+  _RootViewState createState() => _RootViewState();
 }
 
-enum InputMode {
-  camera, gallery
-}
-class BottomController extends GetxController {
-  var selectedTab = 0.obs;
-  var inputMode = InputMode.camera.obs;
+class _RootViewState extends State<RootView> {
+  final BottomController c = Get.put(BottomController());
+  int _selectedItemPosition = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    c.selectedTab.value == 1
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      resizeToAvoidBottomInset: true,
+      extendBody: true,
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+      ),
+      body: Stack(children: [
+        Obx(() {
+          return Visibility(
+            visible: c.selectedTab.value == 1,
+            child: Container(
+              child: Scanner(),
+            ),
+          );
+        }),
+        Obx(() {
+          return Visibility(
+            visible: c.selectedTab.value == 2,
+            child: Container(
+              child: QRGeneratorSharePage(),
+            ),
+          );
+        }),
+        Obx(() {
+          return Visibility(
+            visible: c.selectedTab.value == 3,
+            child: Container(
+              child: HistoryView(),
+            ),
+          );
+        }),
+        Obx(() {
+          return Visibility(
+            visible: c.selectedTab.value == 4,
+            child: Container(
+              child: Center(child: Text("Tab 4")),
+            ),
+          );
+        }),
+      ]),
+      bottomNavigationBar: SnakeNavigationBar.color(
+        // height: 80,
+        behaviour: SnakeBarBehaviour.floating,
+        snakeShape: SnakeShape.circle,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25)),
+        ),
+        padding: const EdgeInsets.all(12),
+        snakeViewColor: Colors.black,
+        selectedItemColor: null,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        currentIndex: _selectedItemPosition,
+        onTap: (index) => setState(() { c.selectedTab.value = index + 1; _selectedItemPosition = index;}),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.camera), label: "Scan"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.create_new_folder_outlined), label: "Create"),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: "About"),
+        ],
+        selectedLabelStyle: const TextStyle(fontSize: 14),
+        unselectedLabelStyle: const TextStyle(fontSize: 10),
+      ),
+    );
+  }
 }
--- lib/scanner.dart
+++ lib/scanner.dart
@@ -9,14 +9,14 @@ import 'DataCacheManager.dart';
 import 'alert_dialog.dart';
 import 'main.dart';
 
-class QRViewExample extends StatefulWidget {
-  const QRViewExample({Key? key}) : super(key: key);
+class Scanner extends StatefulWidget {
+  const Scanner({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _QRViewExampleState();
+  State<StatefulWidget> createState() => _ScannerState();
 }
 
-class _QRViewExampleState extends State<QRViewExample> {
+class _ScannerState extends State<Scanner> {
   Barcode? result;
   QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
@@ -32,60 +32,55 @@ class _QRViewExampleState extends State<QRViewExample> {
 
   @override
   Widget build(BuildContext context) {
-    BottomController c = Get.find();
-
-    return Obx(() {
-      return Stack(
+   return Stack(
         children: [
-          if (c.inputMode.value == InputMode.camera) _buildQrView(context),
-          if (c.inputMode.value == InputMode.camera) Text("S"),
-          Positioned(
-            child: FloatingActionButton(
-                onPressed: () async {
-                  await controller?.toggleFlash();
-                  setState(() {});
-                },
-                child: (Icon(Icons.flash_on, size: 30))),
-            right: 20,
-            bottom: 20,
-          ),
-          Positioned(
-            child: FloatingActionButton(
-                onPressed: () async {
-                  await controller?.flipCamera();
-                  setState(() {});
-                },
-                child: (Icon(Icons.flip_camera_ios, size: 30))),
-            left: 20,
-            bottom: 20,
-          ),
-          Positioned(
-            child: FloatingActionButton(
-                onPressed: () async {
-
-                  var _picker = ImagePicker();
-                  // Pick an image
-                  final XFile? image =
-                      await _picker.pickImage(source: ImageSource.gallery);
-                  final result = await RScan.scanImagePath(image!.path);
-                  if (result.message == null) {
-
-                    AlertPopup.showAlertDialog("Empty", "" ,context);
-
-                  } else {
-                    AlertPopup.showAlertDialog("${result.type}", result.message!, context );
-                  }
-
-
-
-                },
-                child: (Icon(Icons.photo, size: 30))),
-            left: (MediaQuery.of(context).size.width / 2) - 15.0,
-            bottom: 20,
-          ),
+          _buildQrView(context),
+          // Positioned(
+          //   child: FloatingActionButton(
+          //       onPressed: () async {
+          //         await controller?.toggleFlash();
+          //         setState(() {});
+          //       },
+          //       child: (Icon(Icons.flash_on, size: 30))),
+          //   right: 20,
+          //   bottom: 20,
+          // ),
+          // Positioned(
+          //   child: FloatingActionButton(
+          //       onPressed: () async {
+          //         await controller?.flipCamera();
+          //         setState(() {});
+          //       },
+          //       child: (Icon(Icons.flip_camera_ios, size: 30))),
+          //   left: 20,
+          //   bottom: 20,
+          // ),
+          // Positioned(
+          //   child: FloatingActionButton(
+          //       onPressed: () async {
+          //
+          //         var _picker = ImagePicker();
+          //         // Pick an image
+          //         final XFile? image =
+          //             await _picker.pickImage(source: ImageSource.gallery);
+          //         final result = await RScan.scanImagePath(image!.path);
+          //         if (result.message == null) {
+          //
+          //           AlertPopup.showAlertDialog("Empty", "" ,context);
+          //
+          //         } else {
+          //           AlertPopup.showAlertDialog("${result.type}", result.message!, context );
+          //         }
+          //
+          //
+          //
+          //       },
+          //       child: (Icon(Icons.photo, size: 30))),
+          //   left: (MediaQuery.of(context).size.width / 2) - 15.0,
+          //   bottom: 20,
+          // ),
         ],
       );
-    });
   }
 
   Widget _buildQrView(BuildContext context) {
